492315302b4d240d987090d2c3d530fbc22a83a5
hersson ceballos <herssonceballos@gmail.com>
2020-08-08 22:03:11 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -26,7 +26,7 @@ def age_appropriate_birthday(birthday_kids)
       if age < 12 
       puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
     else
-      puts 
+      puts "You are too old for this."
     end
   end
 end
