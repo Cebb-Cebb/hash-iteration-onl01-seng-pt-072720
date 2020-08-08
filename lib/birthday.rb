3df08f045c16dbd7ff4047430235751a21995273
hersson ceballos <herssonceballos@gmail.com>
2020-08-08 21:56:27 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -22,15 +22,6 @@ def age_appropriate_birthday(birthday_kids)
   new_age
 end
 
-def select_winner(passengers)
-  winner = ""
-  passengers.each do |suite, name|
-  if suite == :suite_a && name.start_with?("A")
-    winner = name
-    end
-  end
-  winner
-end
 
 
 
